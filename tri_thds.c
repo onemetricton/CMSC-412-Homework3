d0d45ee2be621459848a4b67470a55194635bead
Zach <zsmith876@gmail.com>
2020-11-15 17:53:37 -0600

    Add files via upload
    Clean up

--- tri_thds.c
+++ tri_thds.c
@@ -7,37 +7,33 @@ pthread_mutex_t lock;
 pthread_cond_t iteration_done;
 int threads_created = 0, thread_counter = 0, num_thds = 3;
 
-void *dispid(void *x) { //function for child processes to execute
+void *dispid(void *x) { //function for child threads to execute
 	int i, num_iter=5;
 	//printf("Testing %d\n",pthread_self());
-	pthread_t *older_sib = (pthread_t *) x;
-	/*
-	if(*older_sib!=0) { //if isn't oldest child thread
-		printf("\n\nthread %d waiting on %d addr %x\n",pthread_self(),*older_sib,older_sib);
-		//pthread_join(*older_sib,NULL); //thread i+1 waits for thread i
-	} //*/
-	printf("\n%d threads created\n",++threads_created);
+	//printf("\n%d threads created\n",++threads_created); //Shared resource
 
+	//The assignment specified for each thread to display its ID *in-turn*
+	//... but I do not track execution order within an iteration
 	for(i=0;i<num_iter;i++) {
-		//pthread_mutex_lock(&lock);
-		//pthread_join(*older_sib,NULL); //thread i+1 waits for thread i
-		//printf("\nPreceded by %d",*older_sib);
-
 		printf("\nPID %d, Thread %d, Iteration no. %d, CPU ID %d\n",getpid(),pthread_self(),i+1,sched_getcpu());
 
+		//pthread_mutex_lock(&lock); //No shared resources to worry about
+		//... EXCEPT for the global variables modified here
+		//... But uncommenting the lock & respective unlock below
+		//... somehow causes unpredicatable behavior
+
+		//Wait until the current iteration is done on all worker threads
 		if(++thread_counter<num_thds) {
 			pthread_cond_wait(&iteration_done,&lock);
+			//Still in the body of the function
+			//... is context switching a concern?
 		}
 
 		thread_counter = 0;
-		pthread_cond_signal(&iteration_done);
 		//pthread_mutex_unlock(&lock);
-		//sched_yield();
+		pthread_cond_signal(&iteration_done);
 	}
 }
-
-//TODO get round robin working, pass the mutex in to dispid instead of older_sib
-//No that didn't work either...
 
 int main(int argc, char* argv[]) {
 	int i, max_thds = 100;
@@ -45,8 +41,9 @@ int main(int argc, char* argv[]) {
 	if(argc > 1) num_thds = strtol(argv[1],NULL,10);
 	if(num_thds>max_thds) num_thds = max_thds;
 
-	pthread_t workers[num_thds], thd_iter=0, older_sib=0, parent=pthread_self();
+	pthread_t workers[num_thds], parent=pthread_self();
 	pthread_attr_t tattr;
+
 	pthread_mutex_init(&lock,NULL);
 	pthread_attr_init(&tattr);
 	pthread_attr_setschedpolicy(&tattr, SCHED_RR);
@@ -54,17 +51,14 @@ int main(int argc, char* argv[]) {
 	printf("Parent thread is %d\n",parent);
 
 	for(i=0;i<num_thds;i++) {
-		//printf("Outer loop iteration %d, older sibling %d\n",i,*older_sib);
-		if(pthread_create(&workers[i],&tattr,(void*) dispid,(void*) &older_sib)) {
+		if(pthread_create(&workers[i],&tattr,(void*) dispid,NULL)) {
 			perror("[*] ERROR [*]: Failed to create/join thread.\n");
 			exit(-1-i); //accounting for i==0, error code gives iteration number
-		} else {
-			if(i>0) older_sib = workers[i-1]; //doesn't work because is passed to pthread_create by reference, changes concurrently as dispid is executed ...
 		}
-		//AFTER pthread_create() IS CALLED, THAT THREAD IS DONE EXECUTING
-		//workers[i] = thd_iter; //just pass in the workers to pthread_create ...
 	}
 
+	//Each child thread only exists within its respective dispid instance
+	//... only the parent has scope in main, but others may run concurrently
 	for(i=0;i<num_thds;i++) {
 		pthread_join(workers[i],NULL); //parent waits for workers
 	}
